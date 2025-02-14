40cedb1c3cc2ec1fa5b16ba53b41ea688101c384
Mettavihari <timotheos.allen@gmail.com>
2025-02-14 22:45:39 +1100

    Remove code specific to linux

--- flake.nix
+++ flake.nix
@@ -16,15 +16,6 @@
           poetry
         ];
         env = {
-          # Workaround in linux: python downloads ELF's that can't find glibc
-          # You would see errors like: error while loading shared libraries: name.so: cannot open shared object file: No such file or directory
-          # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          #   pkgs.stdenv.cc.cc
-          # Add any missing library needed
-          # You can use the nix-index package to locate them, e.g. nix-locate -w --top-level --at-root /lib/libudev.so.1
-          # ];
-
-          # Put the venv on the repo, so direnv can access it
           POETRY_VIRTUALENVS_IN_PROJECT = "true";
           POETRY_VIRTUALENVS_PATH = "{project-dir}/.venv";
 
